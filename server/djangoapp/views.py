3707e61d1cb36da3c9770aabdae78b788ef8f769
Connor <disappearing.now@gmail.com>
2024-09-12 18:48:56 -0400

    Finished logout functionality

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -39,6 +39,7 @@ def login_user(request):
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
+@csrf_exempt
 def logout_request(request):
     """Handles logout request when user logs out"""
     logout(request)
