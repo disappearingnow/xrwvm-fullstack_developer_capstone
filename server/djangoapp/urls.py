919bdca295d305fb2ddee3dca479b8aed1455625
Connor <disappearing.now@gmail.com>
2024-09-14 17:13:16 -0400

    Handle dealer reviews

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -19,9 +19,8 @@ urlpatterns = [
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     #path for fetching dealer details
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    
     # path for dealer reviews view
-
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
