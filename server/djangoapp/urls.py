fb5d58fa3c880e64a806f1ab7f846abcc7420cd8
Connor <disappearing.now@gmail.com>
2024-09-14 17:06:24 -0400

    Added paths for get_dealers

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,12 +10,13 @@ urlpatterns = [
     path(route='register', view=views.registration, name='register'),
     # path for login
     path(route='login', view=views.login_user, name='login'),
-
     # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
-
     #path for fetching cars list
     path(route='get_cars', view=views.get_cars, name ='getcars'),
+    #paths for fetching dealers
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
     # path for dealer reviews view
 
