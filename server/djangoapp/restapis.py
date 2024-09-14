f7e7dd9ff04650f63b2f08f809c633a8b9277ab4
Connor <disappearing.now@gmail.com>
2024-09-14 17:18:32 -0400

    Handle adding reviews

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -28,6 +28,7 @@ def get_request(endpoint, **kwargs):
         # If any error occurs
         print("Network exception occurred")
 
+# Add code for retrieving sentiments
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
@@ -39,8 +40,12 @@ def analyze_review_sentiments(text):
         print("Network exception occurred")
 
 
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
-
-# def post_review(data_dict):
 # Add code for posting review
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
