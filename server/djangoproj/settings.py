97fef77e858edd4b7f9cb44086d6a6637a219021
Connor <disappearing.now@gmail.com>
2024-09-16 18:05:17 -0400

    Fixing linting errors

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = ["https://disappearing-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
+CSRF_TRUSTED_ORIGINS = [
+    'https://disappearing-8000.'
+    'theiadockernext-0-labs-prod-theiak8s-4-tor01.'
+    'proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
