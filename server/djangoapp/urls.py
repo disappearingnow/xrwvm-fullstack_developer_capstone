b7440567d5c1ba2df9817bccada4c22c3c601572
Connor <disappearing.now@gmail.com>
2024-09-15 20:54:03 -0400

    Updated get dealers route

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     #path for fetching cars list
     path(route='get_cars', view=views.get_cars, name ='getcars'),
     #paths for fetching dealers
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     #path for fetching dealer details
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
